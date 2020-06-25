4c31456f47090d59d472d3702ccdebe69489463b
Inna Kutomanov <kutomanova.inna@gmail.com>
2020-06-25 20:01:42 +0300

    chek 1_5

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -9,7 +9,11 @@ function truncate(str, maxlength) {
  if (str.length > maxlength) {
     str = str.slice(0, maxlength - 1) + "…";  
   }
-  return str;
+
+if (maxlength = 0) {
+    str = "";  
+  }
+    return str;
 }
 
 
