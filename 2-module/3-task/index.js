405bfef3c8ac2dc8fea56b08575b84f01d83c390
Inna Kutomanov <kutomanova.inna@gmail.com>
2020-06-27 16:05:34 +0300

    task 2_3

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -10,9 +10,6 @@ let calculator = {
     return this.a * this.b;
   }
 };
-calculator.read(3, 5);
-console.log(calculator.sum()); // 8
-console.log(calculator.mul()); // 15
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
 window.calculator = calculator; // делает ваш калькулятор доступным глобально
