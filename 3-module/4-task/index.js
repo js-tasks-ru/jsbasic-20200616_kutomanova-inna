451a95e8bbeeac02e4a695523dac6661a7977a07
Inna Kutomanov <kutomanova.inna@gmail.com>
2020-07-04 12:44:52 +0300

    check 3_4

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -5,5 +5,20 @@
  * @returns {string}
  */
 function showSalary(users, age) {
-  // ваш код...
+  let filterUsers = users.filter(function(item, index, arrey) {
+
+    if (item.age <= age) {
+      return true;
+    } else {
+      return false;
+    }
+  
+  });
+  let mapUsers = filterUsers.map(function(item, index, arrey) {
+    let resultUser = item.name + ', ' + item.balance;
+    return resultUser;
+  });
+  let joinUsers = mapUsers.join('\n');
+  return joinUsers;
+ 
 }
