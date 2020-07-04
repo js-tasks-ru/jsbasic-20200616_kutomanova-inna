7252cf2c4415d5b491718252492d29cafad0dec9
Inna Kutomanov <kutomanova.inna@gmail.com>
2020-07-04 18:18:24 +0300

    4_1

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -4,5 +4,12 @@
  * @return {HTMLUListElement}
  */
 function makeFriendsList(friends) {
-  // ваш код...
+  let newUl = document.createElement('ul');
+  for (item of friends) {
+    let li = document.createElement ('li');
+    console.log(item.firstName);
+    li.innerHTML = item.firstName + ' ' + item.lastName;
+    newUl.appendChild(li);
+  }
+  return newUl;
 }
