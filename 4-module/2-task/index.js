144e909d1b4fa017327d3a05f7aff91b300d4957
Inna Kutomanov <kutomanova.inna@gmail.com>
2020-07-06 20:34:13 +0300

    check 4_2

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -3,4 +3,10 @@
  * @return {void}
  */
 function makeDiagonalRed(table) {
+    let allTable = document.querySelector('table');
+    
+    for (let i = 0; i < allTable.rows.length; i ++) {
+        allTable.rows[i].cells[i].style.backgroundColor = "red";
+    }
 }
+    
