cf1014836d9fb31de4ed9736f7ee1039d9a35c64
Inna Kutomanov <kutomanova.inna@gmail.com>
2020-07-09 14:12:39 +0300

    4-3

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -3,4 +3,33 @@
  * @param {Element} table
  */
 function highlight(table) {
+    let allTable = table;
+        
+    for (let i = 1; i < allTable.rows.length; i ++) {
+        for (let j = 0; j < allTable.rows[i].cells.length; j ++) {
+            if (j === 3) {
+                if (allTable.rows[i].cells[j].hasAttribute('data-available')) {
+                    if (allTable.rows[i].cells[j].getAttribute('data-available') === "true") {
+                        allTable.rows[i].cells[j].classList.add('available');    
+                    } else {
+                        allTable.rows[i].cells[j].classList.add('unavailable'); 
+                    }
+                } else {
+                    allTable.rows[i].cells[j].setAttribute('hidden', '');
+                }
+            }                    
+            if (j === 2) {
+                if (allTable.rows[i].cells[j].textContent === 'm') {
+                    allTable.rows[i].cells[j].classList.add('male');
+                } else {
+                    allTable.rows[i].cells[j].classList.add('female');
+                } 
+            } 
+            if (j === 1) {
+                if (allTable.rows[i].cells[j].textContent < 18) {
+                    allTable.rows[i].cells[j].style.textDecoration = "line-through";
+                }
+            } 
+        }        
+    }
 }
