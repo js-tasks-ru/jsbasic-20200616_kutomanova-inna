d85fb74f9fc27820bda035281ebb23e93b8a3fe3
Inna Kutomanov <kutomanova.inna@gmail.com>
2020-07-25 11:50:12 +0300

    8-1

--- 8-module/1-task/index.js
+++ 8-module/1-task/index.js
@@ -5,6 +5,7 @@ export default class CartIcon {
     this.render();
 
     this.addEventListeners();
+    this.flagUpdatePosition = 0;
   }
 
   render() {
@@ -39,6 +40,50 @@ export default class CartIcon {
   }
 
   updatePosition() {
-    // ваш код ...
+  
+    if (document.documentElement.clientWidth <= 767) {
+      Object.assign(this.elem.style, {
+        position: '',
+        top: '',
+        left: '',
+        zIndex: ''
+      });
+    } else {
+      let initialTopCoordinate = this.elem.getBoundingClientRect().top + window.pageYOffset;
+      
+        if (window.pageYOffset > initialTopCoordinate) {
+          if (this.flagUpdatePosition == 0) {
+            this.elem.style.position = "fixed";
+      
+            let leftIndent = Math.min(
+              document.querySelector('.container').getBoundingClientRect().right + 20,
+              document.documentElement.clientWidth - this.elem.offsetWidth - 10
+            );
+              
+            Object.assign(this.elem.style, {
+              position: 'fixed',
+              top: '50px',
+              zIndex: 1e3,
+              right: '10px',
+              left: `${leftIndent}px`
+            });
+
+            this.flagUpdatePosition = 1;
+        }
+      
+      } else {
+        this.elem.style.position = "absolute";
+  
+        Object.assign(this.elem.style, {
+          position: '',
+          top: '',
+          left: '',
+          zIndex: ''
+        });
+
+        this.flagUpdatePosition = 0;
+      }
+    }
+        
   }
 }
