59aab61efbb31590036ac82832637e0fc0903ec5
Inna Kutomanov <kutomanova.inna@gmail.com>
2020-07-27 21:38:37 +0300

    8-3

--- 8-module/3-task/index.js
+++ 8-module/3-task/index.js
@@ -6,23 +6,53 @@ export default class Cart {
   }
 
   addProduct(product) {
-    // ваш код
+    let pos_in_List = this.cartItems.findIndex((item) => item.product.name === product.name);
+
+    if (pos_in_List > -1) {
+      this.cartItems[pos_in_List].count += 1;
+
+    } else {
+      this.cartItems.push({ product: product, count: 1 });
+    }
+    this.onProductUpdate(this.cartItems);
   }
 
   updateProductCount(productId, amount) {
-    // ваш код
+    let pos_in_List = this.cartItems.findIndex((item) => item.product.id === productId);
+
+    if (pos_in_List > -1) {
+      this.cartItems[pos_in_List].count += amount;
+
+      if (this.cartItems[pos_in_List].count === 0) {
+        this.cartItems.splice(pos_in_List, 1);
+      }
+
+      this.onProductUpdate(this.cartItems);
+
+    }
   }
 
   isEmpty() {
-    // ваш код
+    if (this.cartItems.length == 0) {
+      return true;
+    }
+    return false;
   }
 
   getTotalCount() {
-    // ваш код
+    let result = this.cartItems.reduce(function (sum, current) {
+      return sum + current.count;
+    }, 0);
+    return result;
   }
 
   getTotalPrice() {
-    // ваш код
+    let result = this.cartItems.reduce(function (sum, current) {
+      return sum + current.count * current.product.price;
+    }, 0);
+
+    return result;
+
   }
 
   onProductUpdate() {
